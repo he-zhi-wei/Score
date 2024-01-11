2493cbdf22f9f10d95be4222689b53c92cdcccec
kangkang <kangkang@hello.com>
2024-01-11 22:53:23 +0800

    hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int a;
+    string b;
+    cout << "What's your name?" << endl;
+    cin >> b;
+    cout << "What's your age?" << endl;
+    cin >> a;
+    cout << "Have a nice day, " << b << " in " << a << endl;
+    return 0; 
+}
